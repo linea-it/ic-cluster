81a9723c2d916953ecb56f63d65dddc302651cec
gabrieloliveira2428 <gabrieloliveira2428@gmail.com>
2022-03-16 07:28:56 +0000

    Last commit before first merge propose

--- notebooks/gavodb.py
+++ notebooks/gavodb.py
@@ -4,6 +4,7 @@ dbtesting = 'postgres://untrusted:untrusted@desdb4.linea.gov.br:5432/gavo'
 
 import numpy as np
 import sqlalchemy
+from astropy.table import Table
 
 class DBManager():
     """
@@ -225,8 +226,11 @@ class DBManager():
             Value of product
         """
         return self.get_property_in_xml(xmlfile, string_in_prop, 'value')
-
-#--------- gabolv_features ---------#
+    
+    
+    #####################################
+    #--------- gabolv_features ---------#
+    #####################################
 
     def get_db_table_columns_names(self, table_name):
         """
@@ -294,4 +298,48 @@ class DBManager():
                 )
             ).fetchall()
         )
-            
+    
+    def table_loading(self,pid, ntable, columns = None, limit = False,verbose = False,
+                  indexs = None, column_indexs_name = None, footprint_table_name = None, sorting_column = None):
+        ''' Assign a table from a especific process to a variable.The rows and columns of the table can be
+            altered according to the args options.
+
+        Args:
+        -----
+            pid (int): process id on DES Science Portal.
+            ntable (int): each process id can contain more than one table associated. So you can choose what you want.
+            columns (string array or None): the columns to add to the table. If None, all columns will be added to the table.
+            limit (integer or False): number of rows to add to the table. If False, all rows will be added.
+            verbose (bool): Print some infos.
+
+            indexs (array): array with values from "column_indexs_name" column that are desired to be select. It's very slow for big arrays.
+            column_indexs_name (str): the name of the column that you want to use to select rows with "indexs" values.
+            - None for both this args gives the complete table.
+
+            footprint_table_name (str): name of the footprint table used for select rows of the table.
+            sorting_column (str): name of the commom column for both table and footprint table utilized to select rows.
+
+        Returns:
+            table_cool (astropy table): the desired table.
+
+        '''
+
+        pid_tables = self.get_tablelist_from_pid(pid)
+        table_name = self.get_tablelist_from_pid(pid)[int(ntable)]
+
+        if columns == None: columns = self.get_db_table_columns_names(table_name)
+
+        table = self.get_db_table_new(table_name, columns, 
+                                     limit = limit, indexs = indexs, column_indexs_name = column_indexs_name,
+                                     footprint_table_name = footprint_table_name, sorting_column = sorting_column)
+
+        table_cool = Table(rows=table.T, names=columns)
+
+        if verbose == True:
+            print(f'Tables in pid {pid}:', pid_tables)
+            print(f'\nTable chosen:',table_name)
+            print(f'\nColumns of table {table_name}: = ', columns,'\n')
+            print(table_cool)
+            print('\nDone!')
+
+        return table_cool
